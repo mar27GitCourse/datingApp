e984a2e0039f6cb8a31f45e4d5fa50ecb66a1baa
Marvin Ch <software14cr@gmail.com>
2021-09-11 09:21:02 -0600

    End of section 5

---
+++ client/src/app/_services/account.service.ts
@@ -0,0 +1,49 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { ReplaySubject } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { User } from '../_models/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AccountService {
+  baseUrl = 'https://localhost:5001/api/';
+  private currentUserSource = new ReplaySubject<User>(1);//one user  ReplaySubject is a type of observable (kind of buffer)
+  currentUser$ = this.currentUserSource.asObservable(); // use $ as convention
+
+  constructor(private http: HttpClient) { }
+  
+  login(model: any) {
+    return this.http.post<User>(this.baseUrl + 'account/login', model).pipe(
+      map((response: User) => {
+        const user = response;
+        if (user) {
+          localStorage.setItem('user', JSON.stringify(user));// store the user in the browser
+          this.currentUserSource.next(user);
+        }
+      })
+    );
+  }
+
+  register(model: any) {
+    return this.http.post<User>(this.baseUrl + 'account/register', model).pipe(
+      map((user: User) => {
+        if (user) {
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currentUserSource.next(user);
+        }
+        return user;// return the user if we needed, but in this case is not neccessary just log in the console the user registered!.
+      })
+    );
+  }
+
+  setCurrenUser(user: User) {
+    this.currentUserSource.next(user);
+  }
+
+  logout() {
+    localStorage.removeItem('user');
+    this.currentUserSource.next(undefined);
+  }
+}
---
+++ client/src/app/nav/nav.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../_models/user';
+import { AccountService } from '../_services/account.service';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit {
+
+  model: any = {}
+  
+  constructor(public accountService: AccountService) { } //inject the account service //vs code auto add the import as sooom as we provide the parameter type
+
+  ngOnInit(): void {
+    
+  }
+  login() {
+    this.accountService.login(this.model).subscribe(response => {
+      console.log(response);      
+    }, error => {
+      console.error(error)
+    });
+  }
+
+  logout() {
+    this.accountService.logout();    
+  }
+}
